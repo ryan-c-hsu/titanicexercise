93ddd53b0c0b9e11016c473d0215f128693cde43
ryan-c-hsu <ryan.c.hsu@gmail.com>
2015-10-09 17:09:03 -0700

    Added Model 1

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # titanicexercise
 
+Ratios - Overall, Men, Women, Class 1, Class 2. Class 3, Embarked 1, Embarked 2, Embarked 3
+
 Excel Data File -
 0. Survived: 1-yes 0-no (Cleanup Done - No changes needed)
 1. Name: Name of Passenger (Delete)
-2. ++Sex: Gender of Passenger (Convert - Gender to Binary)
+2. Sex: Gender of Passenger (Convert - Gender to Binary)
 3. Age: Age of Passenger (Convert - Ages to Bins)
 	If blank, Fill in proportion to other ages/Fill in median age
 4. Sibsp: number of siblings/spouses on board (Convert)	
@@ -13,7 +15,7 @@ Excel Data File -
 7. Fare: cost of ticket (Convert - Prices to Bins)
 	Fill in 0 cost tickets
 8. Cabin: cabin number (Delete)
-9. ++Embarked: Q-Queenstown, C-Cherbourg, S-Southhampton (Convert - Cities to Number)
+9. Embarked: Q-Queenstown, C-Cherbourg, S-Southhampton (Convert - Cities to Number)
 10. Boat: ??? (Delete)
 11. Body: ??? (Delete)
 12. Home.Dest: ??? (Delete)
--- main.py
+++ main.py
@@ -1,14 +1,69 @@
 import pandas as pd
 import numpy as np
+import csv as csv
+from sklearn.ensemble import RandomForestClassifier
 
 # For .read_csv, always use header=0 when you know row 0 is the header row
 data = pd.read_csv('Data/titanic_full.csv', header=0)
 
+# Basic Cleaning
 data['nSex'] = data['sex'].map({'female':0, 'male':1}).astype(int)
 data['nEmbarked'] = data['embarked'].map({'S':0, 'C':1, 'Q':2})
+data.loc[(data.nEmbarked.isnull())] = 0
 data['nFamilySize'] = data['sibsp'] + data['parch']
 
+# Getting Data for Cross Validation
+ratioTable = pd.DataFrame({'actual': pd.Series([0,1,2], index=['overall','men','women'])})
+ratioTable.actual[1] = float(data.survived.sum())/data.shape[0]
+men = data[(data[''])]
 
-print data[nSex][0:5]
-print data[nEmbarked][0:5]
-print data[nFamilySize][0:5]
+# Cleaning for Model 1 (Cleaning Age and Fares)
+median_ages = np.zeros((2,3))
+for i in range(0,2):
+	for j in range(0,3):
+		median_ages[i,j] = data[(data['nSex'] == i) & (data['pclass'] == j+1)]['age'].dropna().median()
+data['nAge'] = data['age']
+for i in range(0,2):
+	for j in range(0,3):
+		data.loc[(data.age.isnull()) & (data.nSex == i) & (data.pclass == j+1),'nAge'] = median_ages[i,j]
+
+median_fare = np.zeros((1,3))
+for i in range(0,1):
+	for j in range(0,3):
+		median_fare[i,j] = data[(data['pclass'] == j+1) & (data['fare'] != 0)]['age'].median()
+data['nFare'] = data['fare']
+for i in range(0,1):
+	for j in range(0,3):
+		data.loc[((data.fare.isnull()) | (data.fare == 0)) & (data.pclass == j+1),'nFare'] = median_fare[i,j]
+
+# data cleaning for model 2 (Converted Ages, Fares into bins)
+
+# data cleaning for model 3 (Added First letter of last name)
+
+traindata = data[['survived','nSex','nEmbarked','nFamilySize','nAge','nFare','pclass']]
+traindata = traindata.astype(int)
+traindata = traindata.values
+
+testdata = data[['nSex','nEmbarked','nFamilySize','nAge','nFare','pclass']]
+testdata = testdata.astype(int)
+testdata = testdata.values
+
+forest = RandomForestClassifier(n_estimators = 1300)
+forest = forest.fit(traindata[0::,1::],traindata[0::,0])
+output = forest.predict(testdata)
+
+output = pd.DataFrame(output)
+output['Gender'] = data.nSex
+
+print output[0].sum()
+print output.shape[0]
+ratio1 = float(output[0].sum())/float(output.shape[0])
+print ratio
+print ratio1
+
+predictions_file = open("model1.csv", "wb")
+open_file_object = csv.writer(predictions_file)
+open_file_object.writerow(["Survived"])
+open_file_object.writerows(zip(output))
+predictions_file.close()
+
